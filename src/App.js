a5ff898c82dd8959a1252001e5ad5bccaf5b797f
jc8080 <jc8080@rit.edu>
2021-04-24 00:38:36 -0400

    Implemented a Keypad Componenet, Display Component, and basic implementation with simple operation checking

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import Display from './componenets/Display';
+import Keypad from './componenets/Keypad';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value:"",
+    } 
+    this.inputKey = this.inputKey.bind(this);
+    this.checkInput = this.checkInput.bind(this);
+  }
 
-    </div>
-  );
+  inputKey(name){
+    const check = this.checkInput(name);
+    if (!check){
+      this.setState({
+        value: this.state.value.concat(name)
+      })
+    }
+  }
+
+  checkInput(name){
+    const operation = ["+", "-"];
+    if (operation.includes(name)){
+      if (!operation.includes(this.state.value.slice(-1))){
+        console.log("true")
+        return false;
+      }
+    } else{
+      return false;
+    }
+    return true;
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <Display value={this.state.value}></Display>
+        <Keypad inputKey={this.inputKey}></Keypad>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
